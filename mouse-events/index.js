ba454033eec4e0b69c24ee22d9ff7b898052cd92
M-Asimm <kpkprince55persia@gmail.com>
2024-02-26 17:04:22 +0500

    task is completed

--- mouse-events/index.js
+++ mouse-events/index.js
@@ -1,15 +1,27 @@
 let body = document.querySelector("body");
+let time = document.querySelector("#time");
+let x_value = document.querySelector("#x-axis");
+let y_value = document.querySelector("#y-axis");
+
 body.addEventListener("click", (e) => {
   console.log(e.clientX, e.clientY);
   getCurrentDate();
+
+  console.log(x_value);
+  console.log(y_value);
+
+  x_value.innerText = "X-Axis:" + e.clientX;
+  y_value.innerText = "X-Axis:" + e.clientY;
 });
 
 function getCurrentDate() {
   let date = new Date();
-  console.log(
+
+  time.textContent =
+    "Time:" +
     date.getHours() +
-      ":" +
-      date.getMinutes() +
-      ":" +
-      date.getSeconds());
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
 }
